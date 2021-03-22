ca3fbe9508aba5b3aa485923621ed61814067f75
Phu Nguyen <phu21994@gmail.com>
2021-03-23 00:57:37 +0700

    Active sound click function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const bankOne = [
   {
@@ -58,22 +58,124 @@ const bankOne = [
   }
 ];
 
+const bankTwo = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+  }
+];
+
 function App() {
   const [count, setCount] = useState(bankOne);
+  const [letter, setLetter] = useState("");
+  
+  
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyUp);
+    return window.removeEventListener("keydown", handleKeyUp);
 
-  const handleOnclick = () => {
+  })
+
+
+  const handleOnclick = (e) => {
+        
+    console.log(e.target.value)
+    
+    const sound = document.getElementById(e.target.value);
+    sound.currentTime = 0;
+    sound.play()
+      .then(()=> setLetter(e.target.id))
+      .catch((err)=> console.log(err))
+
+       
+    
+
 
   }
+
+  const handleKeyUp = (e) => {
+    count.map(({keyCode})=> {
+      if (e.keyCode === keyCode) {
+        const sound = document.getElementById(e.target.value);
+        sound.currentTime = 0;
+        sound.play();
+
+    }
+    
+      
+    
+
+
+    })
+  }
+
+  
   return (
     <div className="App" id="drum-machine">
       <div  id="display">
+        <h2>{letter}</h2>
+        
+        <div>
         {count.map(
-          ({keyCode, keyTrigger, id, url}) => <div  className="drum-pad" id={id} onClick={handleOnclick}>
-            <audio src={url}>{keyTrigger}</audio>
-          </div>)}
+          ({keyCode, keyTrigger, id, url}, index) => 
+          <button  className="drum-pad" id={id} onClick={handleOnclick} onKeyDown={handleKeyUp} value={keyTrigger} key={index}>
+            {keyTrigger}
+            <audio className="clip" id={keyTrigger} src={url}></audio>
+          </button>)}
 
       </div>
-      <div></div>      
+      </div>
+            
     </div>
   );
 }
